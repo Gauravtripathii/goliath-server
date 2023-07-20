e4c8d9fa70468329124bbbdd4001aaf9639c28c4
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-20 18:56:51 +0530

    user init

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import userRoutes from "./routes/user.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+
+app.use("/user", userRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://golaithAdmins:golaithAdmins0@cluster0.zmupedm.mongodb.net/?retryWrites=true&w=majority";
