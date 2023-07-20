b1a56c59eaf4f03675fda7299ecefcc395804d46
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-20 18:50:23 +0530

    database connection established

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://golaithAdmins:golaithAdmins0@cluster0.zmupedm.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log("Connected to Goliath Database!"))
+  )
+  .catch((error) => console.log(error.message));
+
+
