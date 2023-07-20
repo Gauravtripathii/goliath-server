927202e48f148d1708c95366fb0228b9d7427fab
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-20 19:48:43 +0530

    user schema

--- models/users.js
+++ models/users.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
-    Name:String ,
-    Username:String ,
-    Email_id:String ,
-    password:String ,
-    Toa:[String] ,
-    wishlist:[String] ,
-    g_purchase:[String] ,
-    generes_played:[String] ,
-    createdAT:{
-        type:Date ,
-        default:new Date()
-    }
-})
+const userSchema = mongoose.Schema({
+  Name: String,
+  Username: String,
+  Email_id: String,
+  password: String,
+  Toa: [String],
+  wishlist: [String],
+  g_purchase: [String],
+  generes_played: [String],
+  createdAT: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const UserData = mongoose.model('UserData', userSchema);
+export default UserData;
